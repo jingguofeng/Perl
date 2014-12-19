fe59cc43db587b8527523a1636b4b131a2d3e73b
jingguofeng <everjgfeng@gmail.com>
2014-12-19 11:27:27 -0600

    add more

--- variable_scalar.pl
+++ variable_scalar.pl
@@ -6,3 +6,19 @@ print $num."\n";
 
 my $string = "world";
 print "my string: ".$string;
+
+my $str1 = "4G";
+my $str2 = "4h";
+
+print $str1.$str2."\n";
+
+print $str1 + $str2;
+print "\n";
+
+#The classic error
+print "yes" == "no"; # "1" with two warning; both values evaluate to 0 when used as numbers
+
+#Numerical operators: <, >, <=, >=, ==, !=, <=>, +, *
+#String operaotors: lt, gt, le, ge, eq, ne, cmp, ., x
+
+
